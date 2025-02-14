fd9e774ec9a56b4d29176cd1f5d15308ec615a6c
Abdulrahman Khaled <abdulrahmankhaled0000@gmail.com>
2025-02-14 18:38:44 +0200

    add api

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,6 +1,8 @@
 
 async function page() {
-
+  const response= await fetch('https://fakestoreapi.com/products')
+  const data= await response.json()
+  console.log(data);
     return (
       <div>
         <h1>Products</h1>
